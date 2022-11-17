2dfeb5af309213dd566e9daa95835e8a78aed72c
sugtao4423 <sugtao4423@gmail.com>
2022-11-18 04:25:11 +0900

    Settingsリファクタリング

--- settings/index.tsx
+++ settings/index.tsx
@@ -1,18 +1,14 @@
-function dateFormatSettings(props) {
-  return (
-    <Page>
-      <Select
-        label="Date Format"
-        settingsKey="dateFormat"
-        options={[
-          { name: 'yyyy/MM/dd E' },
-          { name: 'E, MM/dd/yyyy' },
-          { name: 'E, dd/MM/yyyy' },
-          { name: 'E, dd MM月 yyyy' },
-        ]}
-      />
-    </Page>
-  )
-}
-
-registerSettingsPage(dateFormatSettings)
+registerSettingsPage((_: SettingsComponentProps) => (
+  <Page>
+    <Select
+      label="Date Format"
+      settingsKey="dateFormat"
+      options={[
+        { name: 'yyyy/MM/dd E' },
+        { name: 'E, MM/dd/yyyy' },
+        { name: 'E, dd/MM/yyyy' },
+        { name: 'E, dd MM月 yyyy' },
+      ]}
+    />
+  </Page>
+))
